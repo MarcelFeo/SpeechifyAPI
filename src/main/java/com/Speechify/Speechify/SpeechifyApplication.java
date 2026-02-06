8bd9b918ab44b9df31ca676e101938dc7830fc6d
MarcelFeo <marcelfeo29@gmail.com>
2026-02-06 18:34:08 -0300

    start project

---
+++ src/main/java/com/Speechify/Speechify/SpeechifyApplication.java
@@ -0,0 +1,13 @@
+package com.Speechify.Speechify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeechifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeechifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Speechify/Speechify/SpeechifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Speechify.Speechify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeechifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
